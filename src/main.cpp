bd9813536fa1731d5429864413f6c49b6e177865
Doctor Rosenberg <doctorrosenberg3@gmail.com>
2026-05-18 16:11:31 +0700

    Update main.cpp
    Updated for a new function (v0.2)

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,28 @@ int main() {
  std::cout << "Type in your time!! in secs\n"; 
  
  std::unique_ptr<int> numbers = std::make_unique<int>();
+ std::unique_ptr<char> TimerChoice = std::make_unique<char>();
  
  std::cin >> *numbers;
  std::cout << "\n";
  
- timerfunc(*numbers);
+ std::cout << "Type of a timer? \n" << "A. From zero to the top. \nB. From the top to zero. \n";
+ 
+ std::cin >> *TimerChoice;
+ std::cout << "\n";
+ 
+ switch(*TimerChoice) {
+  case 'a':
+  case 'A':
+   timerfuncDOWNTOUP(*numbers);
+   break;
+  case 'b':
+  case 'B':
+   timerfuncUPTODOWN(*numbers);
+   break;
+  default:
+   std::cerr << "Have a nice day. \n";
+ }
  
  std::cout << "The end!";
 }
