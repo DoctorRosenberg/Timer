03181105988bb8c2e82eda3dd552ff3107bab154
Doctor Rosenberg <doctorrosenberg3@gmail.com>
2026-05-18 16:12:30 +0700

    Update timerfunc.cpp
    Changed for a new function (v0.2)

--- src/timerfunc.cpp
+++ src/timerfunc.cpp
@@ -5,9 +5,17 @@
 
 #include "timerfunc.hpp"
 
-void timerfunc(int seconds) {
+void timerfuncDOWNTOUP(const int seconds) {
  for (int c = 0; c < seconds; c++) { //oh my fucking god is this a c++ reference
   std::cout << c << "\n";
   std::this_thread::sleep_for(std::chrono::seconds(1));
  }
 }
+
+void timerfuncUPTODOWN(const int seconds) {
+ for (int c = seconds; c > 0; c--) { //oh my fucking god is this a c-- reference
+  std::cout << c << "\n";
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+ }
+}
+
